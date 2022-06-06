08ad0f8112c74458449f7f61cbb34417ed2d33ed
rawwr <akshayace0302@gmail.com>
2022-06-07 01:23:27 +0530

    React fragments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 import AddUser from "./Components/Users/AddUser";
 import UsersList from "./Components/Users/UserList";
 import { v4 as uuidv } from "uuid";
@@ -14,10 +14,10 @@ function App() {
     });
   };
   return (
-    <div>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
   );
 }
 
